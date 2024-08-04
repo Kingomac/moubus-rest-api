4202177505cfe6e22f1a9f19189d136b9a525e03
Kingomac <kingomacyt@gmail.com>
2024-08-04 12:26:40 +0200

    More test cases

--- src/main/java/com/mario/dev/moubus/users/repository/TravelerMoubusRepository.java
+++ src/main/java/com/mario/dev/moubus/users/repository/TravelerMoubusRepository.java
@@ -1,12 +1,14 @@
 package com.mario.dev.moubus.users.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import com.mario.dev.moubus.users.entity.TravelerMoubus;
 
 @Repository
+@RepositoryRestResource(path = "travelersMoubus", collectionResourceRel = "travelersMoubus")
 public interface TravelerMoubusRepository extends
         JpaRepository<TravelerMoubus, Long> {
 
--- src/main/java/com/mario/dev/moubus/users/repository/TravelerXuntaRepository.java
+++ src/main/java/com/mario/dev/moubus/users/repository/TravelerXuntaRepository.java
@@ -1,12 +1,14 @@
 package com.mario.dev.moubus.users.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import com.mario.dev.moubus.users.entity.TravelerXunta;
 
 @Repository
+@RepositoryRestResource(path = "travelersXunta", collectionResourceRel = "travelersXunta")
 public interface TravelerXuntaRepository extends JpaRepository<TravelerXunta, Long> {
 
 }
